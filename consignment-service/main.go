72965db4d8d836b84a6b62b8aeeaec73da6ed7e2
Jagmal <jagmal@Jagmals-MacBook-Pro.local>
2019-06-26 17:59:05 +0530

    Docker compose build not working where attempting to build using multiple go files

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -8,6 +8,7 @@ import (
 
 	// Import the generated protobuf code
 	pb "github.com/jagmal/shippy/consignment-service/proto/consignment"
+	vesselProto "github.com/jagmal/shippy/vessel-service/proto/vessel"
 	"github.com/micro/go-micro"
 )
 
@@ -46,13 +47,25 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea
 type service struct {
-	repo repository
+	repo         repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity: int32(len(req.Containers))
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	req.VesselId = vesselResponse.Vessel.Id
 
 	// Save our consignment
 	consignment, err := s.repo.Create(req)
@@ -84,7 +97,9 @@ func main() {
 
 	srv.Init()
 
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	vesselClient := vesselProto.NewVesselServiceClient("shippy.service.vessel", srv.Client())
+
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
 	if err := srv.Run(); err != nil {
 		log.Fatalf("Error in runnin server: %v", err)
